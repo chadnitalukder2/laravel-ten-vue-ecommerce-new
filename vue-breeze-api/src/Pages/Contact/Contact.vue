00a8595606aa66089dd490731c7d261e707e2c9c
Puja <137037747+chadnitalukder2@users.noreply.github.com>
2024-04-02 04:11:39 +0600

    product page

--- vue-breeze-api/src/Pages/Contact/Contact.vue
+++ vue-breeze-api/src/Pages/Contact/Contact.vue
@@ -4,34 +4,38 @@
 
 <template>
     <div class="container" >
-        <div class="contact_page" style="display: flex; justify-content: space-between;">
-            <div class="contact_page_left">
-                <div>
-                    <label>Name</label>
-                    <input type="text" placeholder="enter your name">
+        <div style="background:#fff ; width: 70%; margin: 0 auto; padding: 35px; box-shadow: 0 0 30px 0 rgba(0, 0, 0, 0.1); border-radius: 5px;">
+
+            <div class="contact_page" >
+                <div class="contact_page_left" style="flex-basis: 50%;">
+                    <div class="contact_info" >
+                        <label style="font-weight: bold;">Name : </label>
+                        <input type="text" placeholder="enter your name">
+                    </div>
+                    <div class="contact_info">
+                        <label style="font-weight: bold;">Phone : </label>
+                        <input type="number" placeholder="enter your name">
+                    </div>
                 </div>
-                <div>
-                    <label>Phone</label>
-                    <input type="number" placeholder="enter your name">
+                <div class="contact_page_right" style="flex-basis: 50%;">
+                    <div class="contact_info">
+                    <label style="font-weight: bold;">Email : </label>
+                    <input type="email" placeholder="enter your name">
                 </div>
+                <div class="contact_info">
+                    <label style="font-weight: bold;">Address : </label>
+                    <textarea type="text" placeholder="enter your address"></textarea>
+                </div>
+                </div>
+
             </div>
-            <div class="contact_page_right">
-                <div>
-                <label>Email</label>
-                <input type="email" placeholder="enter your name">
-            </div>
-            <div>
-                <label>Address</label>
-                <textarea type="text" placeholder="enter your name"></textarea>
-            </div>
+            
+            <div class="contact_info" style="text-align: left; padding-bottom: 0px;">
+                <label style="font-weight: bold;">Message : </label>
+                <textarea type="text" placeholder="enter your massage" rows="5"></textarea>
             </div>
 
         </div>
-        
-        <div>
-                <label>Message</label>
-                <textarea type="text" placeholder="enter your name"></textarea>
-            </div>
     </div>
 </template>
 
@@ -45,11 +49,24 @@
   .contact_page{
     display: flex;
     justify-content: space-between;
-    width: 65%;
     margin: 0 auto;
-    padding: 20px 30px;
-    background: #fff;
+    gap: 50px;
+    text-align: left;
+
   }
+  .contact_info{
+        padding-bottom: 15px;
+        input, textarea{
+            width: 100%;
+            padding: 12px 20px;
+            margin: 8px 0;
+            display: inline-block;
+            border: 1px solid #ccc;
+            box-sizing: border-box;
+            border-radius: 8px;
+
+        }
+    }
 }
 
 
