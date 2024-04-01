a97039307c06f74cd8c9dd38dd2c5ad21a445d71
Puja <137037747+chadnitalukder2@users.noreply.github.com>
2024-04-02 04:21:03 +0600

    contact page

--- vue-breeze-api/src/Pages/Contact/Contact.vue
+++ vue-breeze-api/src/Pages/Contact/Contact.vue
@@ -4,6 +4,8 @@
 
 <template>
     <div class="container" >
+        <h1>Contact Page</h1>
+        <hr style="border: 1px solid rgb(0, 150, 136); width: 25%; margin-bottom: 40px;">
         <div style="background:#fff ; width: 70%; margin: 0 auto; padding: 35px; box-shadow: 0 0 30px 0 rgba(0, 0, 0, 0.1); border-radius: 5px;">
 
             <div class="contact_page" >
@@ -46,6 +48,17 @@
   text-align: center;
   background: #d7d1d130;
 
+  h1{
+    font-weight: 600;
+    text-transform: uppercase;
+    margin-bottom: 20px;
+    margin: 0 auto;
+    text-align: center;
+    padding: 10px;
+    margin-top: 28px;
+    font-family: Poppins, sans-serif;
+  }
+
   .contact_page{
     display: flex;
     justify-content: space-between;
@@ -64,6 +77,12 @@
             border: 1px solid #ccc;
             box-sizing: border-box;
             border-radius: 8px;
+            font-size: 15px;
+
+            &:focus-visible{
+                border: 1px solid #62c7af;
+                outline: none;
+            }
 
         }
     }
