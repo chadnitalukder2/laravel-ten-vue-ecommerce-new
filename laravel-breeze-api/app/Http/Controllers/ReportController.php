6dbf4f739d4c1f039c4d723c71e2eb6681187215
Puja <137037747+chadnitalukder2@users.noreply.github.com>
2024-04-19 21:24:25 +0600

    product table

--- laravel-breeze-api/app/Http/Controllers/ReportController.php
+++ laravel-breeze-api/app/Http/Controllers/ReportController.php
@@ -10,7 +10,7 @@ use Illuminate\Http\Request;
 class ReportController extends Controller
 {
     function all_product(){
-        $products = Product::orderBy('id', 'desc')->get();
+        $products = Product::with('category', 'brand')->orderBy('id', 'desc')->get();
         return response()->json([
             'products' => $products
         ], 200);
--- vue-breeze-api/src/Pages/Admin/dashboard/dashboard.vue
+++ vue-breeze-api/src/Pages/Admin/dashboard/dashboard.vue
@@ -19,6 +19,7 @@ onMounted(async () => {
 const allProduct =  async () =>{
   let response = await axios.get("/api/all_product");
   products.value = response.data.products;
+  console.log('product', products.value)
 }
 
 const totalAmount = async () => {
@@ -89,6 +90,74 @@ const getUser = async () => {
           </div>
         </div>
       </div>
+
+      <div class="overView">
+        <h1>Latest Products</h1>
+        <div style="width: 100%; border-bottom: 1px solid #857f7f30;"  ></div>
+        <table id="customers">
+        <tr>
+          <th># ID</th>
+          <th>Name</th>
+          <th>Price</th>
+          <th>Quantity</th>
+          <th>Category Name</th>
+          <th>Brand Name</th>
+          <th>Image</th>
+        </tr>
+        <tbody v-for="item in products" :key="item.id">
+          <tr>
+            <td style="color: blue">
+              <router-link
+                :to="{ name: 'edit-product', params: { id: item.id } }"
+                ># {{ item.id }}
+              </router-link>
+            </td>
+            <td>{{ item.product_name }}</td>
+            <td>{{ item.product_price }}</td>
+            <td>{{ item.product_quantity }}</td>
+            <td>{{ item.category.category_name }}</td>
+            <td>{{ item.brand.brand_name }}</td>
+            <td style="width: 70px; height: 60px">
+              <img :src="item.product_img" style="width: 100%; height: 100%" />
+            </td>
+
+          </tr>
+        </tbody>
+      </table>
+      </div>
+
+      <div class="overView">
+        <h1>Latest Orders</h1>
+        <div style="width: 100%; border-bottom: 1px solid #857f7f30;"  ></div>
+        <table id="customers">
+          <tr>
+                <th># ID</th>
+                <th>User Id</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Address</th>
+                <th>Phone</th>
+                <th>Total Amount</th>
+                <th>Order Status</th>
+                <th>Payment Status</th>
+            </tr>
+            <tbody v-for="item in total_amount" :key="item.id">
+                <tr>
+                    <td style=""># {{ item.id }}</td>
+                    <td>{{ item.user_id }}</td>
+                    <td>{{ item.name }}</td>
+                    <td>{{ item.email }}</td>
+                    <td>{{ item.address }}</td>
+                    <td>{{ item.phone }}</td>
+                    <td>{{ item.total_amount }}</td>
+                    <td>{{ item.order_status }}</td>
+                    <td>{{ item.payment_status }}</td>
+                   
+                </tr>
+            </tbody>
+      </table>
+      </div>
+
     </div>
 </template>
 
@@ -101,7 +170,7 @@ const getUser = async () => {
   background: #d7d1d130;
 
   .overView{
-    margin: 50px;
+    margin: 30px;
     background: #fff;
     border-radius: 6px;
     padding: 20px;
@@ -110,7 +179,7 @@ const getUser = async () => {
     h1{
       margin: 0px;
       text-align: left;
-      font-size: 20px;
+      font-size: 22px;
       color: #343232;
       margin-bottom: 10px;
     }
@@ -122,7 +191,7 @@ const getUser = async () => {
       border: 1px solid #857f7f30;
       margin-top: 15px;
       border-radius: 5px;
-      padding: 10px;
+      padding: 12px;
       .details{
         h1{
           margin-bottom: 6px;
@@ -138,10 +207,65 @@ const getUser = async () => {
         padding-top: 14px;
         i{
           color: #767575;
-          font-size: 18px;
+          font-size: 20px;
         }
       }
     }
   }
 }
+
+table {
+  border-radius: 6px;
+  overflow: hidden;
+  margin-top: 20px;
+}
+
+.table-box {
+  padding: 50px;
+  border-radius: 8px;
+
+  .btn {
+    text-align: right;
+    padding-bottom: 10px;
+    button {
+      padding: 10px 20px;
+      border: 1px solid #ddd;
+      background: #189877;
+      border-radius: 6px;
+      cursor: pointer;
+      a {
+        text-decoration: none;
+        font-size: 16px;
+        font-weight: 500;
+        color: #fff;
+      }
+    }
+  }
+}
+
+#customers {
+  font-family: Arial, Helvetica, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+#customers td,
+#customers th {
+  border: 1px solid #f3ededad;
+  padding: 15px 15px;
+  text-align: left;
+}
+
+#customers tr:nth-child(even) {
+  background-color: #f2f2f2;
+}
+
+#customers th {
+  padding-top: 20px;
+  padding-bottom: 20px;
+  text-align: left;
+  background-color: rgb(237 236 236 / 68%);
+  color: #444;
+}
+
 </style>
