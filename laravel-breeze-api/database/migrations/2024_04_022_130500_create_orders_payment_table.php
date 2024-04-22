451396970b66ceafef86bdb321f94b84d5a5e241
Puja <137037747+chadnitalukder2@users.noreply.github.com>
2024-04-22 20:42:55 +0600

    payment table

---
+++ laravel-breeze-api/database/migrations/2024_04_022_130500_create_orders_payment_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders_payment', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name');
+            $table->string('email');
+            $table->string('address');
+            $table->string('phone');
+
+            $table->integer('amount');
+            $table->string('currency');
+            $table->string('status')->default('pending');
+            $table->string('transaction_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
